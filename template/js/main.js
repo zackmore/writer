0f826db93001319889e4c0c3b3255d8b341a10e3
zackmore <zackmoredev@gmail.com>
2014-03-10 09:43:39 +0800

    css minify, javascript compatibilize

--- template/js/main.js
+++ template/js/main.js
@@ -1,9 +1,11 @@
 (function(){
+    // img click to open new page
     imgs = document.querySelectorAll('#article img');
     for(var i=0;i<imgs.length;i++){
         imgs[i].style.cursor = 'pointer';
         imgs[i].addEventListener('click', function(e){
-            window.open(e.srcElement.src);
+            var elem = e.srcElement || e.target;
+            window.open(elem.src);
         });
     }
 })();
